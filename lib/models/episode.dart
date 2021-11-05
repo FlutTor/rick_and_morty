5d62bac300ce7f786fda91ce6609510c5270bb9a
cerberodev <hi@cerbero.dev>
2021-11-04 22:13:41 -0500

    Add models of Location/Episode/Character

---
+++ lib/models/episode.dart
@@ -0,0 +1,11 @@
+class Episode {
+  Episode({
+    required this.name,
+    required this.airDate,
+    required this.episode,
+  });
+
+  String name;
+  String airDate;
+  String episode;
+}
---
+++ lib/models/location.dart
@@ -0,0 +1,7 @@
+class Location {
+  Location({required this.name, required this.type, required this.dimension});
+
+  String name;
+  String type;
+  String dimension;
+}
