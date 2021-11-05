27fa764911fdea7f4ce748cbefa7fe49d81cc873
cerberodev <hi@cerbero.dev>
2021-11-04 22:12:35 -0500

    Refactor: THEME for required brightness in colorSchemeDark

--- lib/const/theme.dart
+++ lib/const/theme.dart
@@ -4,6 +4,7 @@ ThemeData darkTheme() {
   return ThemeData(
     appBarTheme: const AppBarTheme(color: Colors.grey),
     colorScheme: ColorScheme.fromSwatch(
+      brightness: Brightness.dark,
       accentColor: Colors.blueGrey,
     ),
     brightness: Brightness.dark,
@@ -16,5 +17,6 @@ ThemeData ligthTheme() {
     colorScheme: ColorScheme.fromSwatch(
       accentColor: Colors.red[900],
     ),
+    brightness: Brightness.light,
   );
 }
