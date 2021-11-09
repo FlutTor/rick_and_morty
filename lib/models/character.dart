e213e0a5c9cf6fc1c815606e74d30569babe2c73
cerberodev <hi@cerbero.dev>
2021-11-08 22:44:29 -0500

    Fix Character & consumer API

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -3,13 +3,13 @@ class Character {
     required this.name,
     required this.species,
     required this.gender,
-    required this.urlImage,
+    required this.image,
     required this.status,
   });
 
   final String name;
   final String species;
   final String gender;
-  final String urlImage;
+  final String image;
   final String status;
 }
--- lib/repository/character.dart
+++ lib/repository/character.dart
@@ -1,39 +1,36 @@
-// ignore_for_file: avoid_dynamic_calls
+// ignore_for_file: avoid_dynamic_calls, prefer_final_locals
 
 import 'dart:convert';
-import 'package:http/http.dart';
-import 'package:rick_and_morty/models/character.dart';
+import 'package:http/http.dart' as http;
+import 'package:rick_and_morty/models/models.dart';
 
-class CharacterApi {
-  List<Character> characters = [];
+Future<List<Character>> getCharacters() async {
+  final url = Uri.parse(
+      'https://rickandmortyapi.com/api/character/1,183,24,34,4,45,65,72,81,98');
+  final response = await http.get(url);
 
-  Future<List<Character>> getCharacters() async {
-    final url = Uri.parse('https://rickandmortyapi.com/api/character/');
-    final response = await get(url);
+  final responseJson = jsonDecode(response.body.toString()) as List;
+  var charactersList = createCharacterList(responseJson);
+  return charactersList;
+}
 
-    final responseJson = jsonDecode(response.body.toString()) as List;
-    final charactersList = createCharacterList(responseJson);
-    return charactersList;
+List<Character> createCharacterList(List data) {
+  var lists = <Character>[];
+  for (var i = 0; i < data.length; i++) {
+    final name = data[i]['name'] as String;
+    final status = data[i]['status'] as String;
+    final species = data[i]['species'] as String;
+    final gender = data[i]['gender'] as String;
+    final image = data[i]['image'] as String;
+
+    final characters = Character(
+      name: name,
+      gender: gender,
+      species: species,
+      status: status,
+      image: image,
+    );
+    lists.add(characters);
   }
-
-  List<Character> createCharacterList(List data) {
-    final lists = <Character>[];
-    for (var i = 0; i < data.length; i++) {
-      final name = data[i]['name'] as String;
-      final status = data[i]['status'] as String;
-      final species = data[i]['species'] as String;
-      final gender = data[i]['gender'] as String;
-      final urlImage = data[i]['urlImage'] as String;
-
-      final characters = Character(
-        name: name,
-        gender: gender,
-        species: species,
-        status: status,
-        urlImage: urlImage,
-      );
-      lists.add(characters);
-    }
-    return lists;
-  }
+  return lists;
 }
